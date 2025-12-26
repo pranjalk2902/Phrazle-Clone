5c8d9118fe24720808952036adb2819ac56b31ab
pranjalk2902 <pranjalk2902@gmail.com>
2025-12-26 22:49:54 +0530

    New phrase through logic.js

--- logic.js
+++ logic.js
@@ -1,6 +1,6 @@
-const phrase = 'devastating circumstances falling over';
+// const phrase = 'devastating circumstances falling over';
 // const phrase = 'my achilles heel';
-// const phrase = 'thumbs up';
+const phrase = 'thumbs up';
 const phraseTrimmed = phrase.replace(/ /g,'');
 const phraseContainer = document.getElementsByClassName('board-container');
 // const guessedWordsList = [];
@@ -332,3 +332,4 @@ function generateColours(phrase, guess) {
     return wordList;
 
     }
+
